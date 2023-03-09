e5319bddbe2e826717363927e7e6a6a0c9e00cd7
Benoît Paris <benoit.paris.753@gmail.com>
2023-03-09 15:52:36 +0100

    General cleanup

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ import tensorflowjs as tfjs
 
 
 resolution = 92
-ray_params = {
-    'mouse_coords': jnp.array([0.5, 0.5]),
-    'time': jnp.array([0])
-}
+ray_params = {}
+
 
 def rot(a):
     s = jnp.sin(a)
@@ -18,15 +16,16 @@ def rot(a):
          [s,  c,  0],
          [0,  0,  1]])
 
-def cube_points(p, s):
-    p_sec = jnp.absolute(p)-s
-    return jnp.linalg.norm(p_sec) - 0.25
+
+def cube_vertex_spheres(pos):
+    point = jnp.array([1.0, 1.0, 1.0])
+    points = jnp.absolute(pos) - point
+    return jnp.linalg.norm(points) - 0.25
 
 
 def get_dist(p):
-    vec_1 = jnp.array([1.0, 1.0, 1.0])
-    d = cube_points(p, vec_1)
-    return d
+    return cube_vertex_spheres(p)
+
 
 def normalize(v):
     return v / jnp.linalg.norm(v)
@@ -48,14 +47,14 @@ def get_color(x, y):
 
 def ray_march(ro, rd):
     distance = 0.0
-    for i in range(10):
+    for i in range(20):
         distance += get_dist(ro + rd * distance)
     return distance
 
 
 def ray_color(u, v, xs):
     center = jnp.array([0.0, 0.0, 0.0])
-    ro_0 = jnp.array([3.0, 0.0, 0.0])
+    camera_init = jnp.array([3.0, 0.0, 0.0])
     sun = normalize(jnp.array([1.0, 2.0, 3.0]))
 
     #     // where is the mouse?
@@ -64,13 +63,13 @@ def ray_color(u, v, xs):
     # ro.yz *= Rot(-m.y*PI+1.);
     # ro.xz *= Rot(-m.x*TAU + iTime);
 
-    ro = jnp.matmul(rot(xs), ro_0)
+    camera = jnp.matmul(rot(xs/5), camera_init)
 
     # rd = get_ray_dir(u, v, ro, center, xs)
-    rd = get_ray_dir(u, v, ro, center, 1.0)
+    rd = get_ray_dir(u, v, camera, center, 1.0)
 
-    d = ray_march(ro, rd)
-    at_surface = ro + rd * d
+    d = ray_march(camera, rd)
+    at_surface = camera + rd * d
     normal = normalize(jax.grad(get_dist)(at_surface))
 
     # second pass
@@ -79,34 +78,21 @@ def ray_color(u, v, xs):
     # at_surface_2 = at_surface + reflection * d_2
     # normal_2 = normalize(jax.grad(get_dist)(at_surface_2))
 
-    light = jnp.dot(normal, sun)
-    # light = jnp.dot(normal_2, sun)
+    light = (jnp.dot(normal, sun) + 1) / 2
+    # light = (jnp.dot(normal_2, sun) + 1) / 2
 
-    # on clip pour être dans la range; il faudra retirer
-    return jnp.clip(light, 0.0, 1.0)
-    # return jnp.clip(d, 0.0, 1.0)
-    # return d
+    # return jnp.remainder(light, 1.0)
+    # return jnp.clip(light, 0.0, 1.0)
+    return light
 
 
 def main(ray_params, xs):
-    print('#######')
-    print(xs)
-    print('#######')
     cxr = jnp.linspace(-1.0, 1.0, num=resolution, endpoint=False)
     cyr = jnp.linspace(-1.0, 1.0, num=resolution, endpoint=False)
 
-
-
     ray_colors = jax.vmap(jax.vmap(ray_color, (0, None, None), 0), (None, 0, None), 1)
 
-    couter = ray_colors(cxr, cyr, xs[0])
-
-# necessary?
-    out = jnp.dstack((couter, couter, couter))
-    print(out.shape)
-    print(out)
-
-    return out
+    return ray_colors(cxr, cyr, xs[0])
 
 main(ray_params, [0.9])
 
