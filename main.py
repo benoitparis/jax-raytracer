b31b3cdd57e0b3fab7f9916be799150a723a744f
ben <benoit.paris.753@gmail.com>
2025-11-17 09:27:45 +0100

    regroup lighting

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 import jax
 import jax.numpy as jnp
-from jax import lax
-from jax import nn
 import tensorflow as tf
 import tensorflowjs as tfjs
 
 
 resolution = 512
+
+
+def sphere(position, size):
+    return jnp.linalg.norm(position) - size
+
+
+def cube(position, size):
+    return jnp.max(jnp.abs(position) - size)
+
+
+def union(d1, d2, k=0.1):
+    # smooth minimum, as min is not differentiable; root method
+    # k the typical distance over which to smooth
+    k *= 2.0
+    x = d2 - d1
+    return 0.5 * (d1 + d2 - jnp.sqrt(x * x + k * k))
+
+
+def distance_function(position, time):
+    return union(
+        cube(position, 0.85),
+        sphere(position + 2.0 * jnp.sin(time), 1.0)
+    )
+
+
+def normalize(v):
+    return v / jnp.linalg.norm(v)
+
+
+
+def get_ray_direction(u, v, camera_direction):
+    vertical = jnp.array([0.0, 0.0, 1.0])
+    r = normalize(jnp.cross(vertical, camera_direction))
+    u_1 = jnp.cross(camera_direction, r)
+    i = camera_direction + u*u_1 + v*r
+    return normalize(i)
 
 
 def rotate_z(a):
@@ -18,78 +52,49 @@ def rotate_z(a):
          [s,  c,  0],
          [0,  0,  1]])
 
-def sphere(position, size):
-    return jnp.linalg.norm(position) - size
 
-
-def cube(position, size):
-    return jnp.max(jnp.abs(position) - size)
-
-
-def union(d1, d2, k=0.1):
-    # smooth minimum, as min is not differentiable; with a root
-    # k the typical distance over which to smooth
-    k *= 2.0
-    x = d2 - d1
-    return 0.5 * (d1 + d2 - jnp.sqrt(x * x + k * k))
-
-
-def distance_function(position, time):
-#     return jnp.linalg.norm(jnp.mod(position, 4.0) - 2.0) - 0.5
-#     return sphere(position + 2.0, 1.0) - 1.0
-#     return cube(position + 2.0, 1.0) - 1.0
-    return union(cube(position, 0.0) - 1.0, sphere(position + 2.0 *jnp.sin(time), 1.0)) # marche pas..
-#     return jnp.max(position + 2.0) - 1.0
-
-
-def normalize(v):
-    return v / jnp.linalg.norm(v)
+def get_camera(x, y):
+    position = jnp.array([-2.0, 4.0, 0.0])
+    position = jnp.matmul(rotate_z(5 * (x/resolution - 0.5)), position)
+    position = position + jnp.array([0.0, 0.0, -10 * (y/resolution - 0.5)] )
+    direction = normalize(-position)
+    return position, direction
 
 
 def reflect(incident, normal):
     return incident - 2 * jnp.dot(normal, incident) * normal
 
 
-def get_ray_direction(u, v, camera_direction):
-    vertical = jnp.array([0.0, 0.0, 1.0])
-    r = normalize(jnp.cross(vertical, camera_direction))
-    u_1 = jnp.cross(camera_direction, r)
-    i = camera_direction + u*u_1 + v*r
-    return normalize(i)
-
-
-def get_camera(txy):
-    position = jnp.array([6.0, 0.0, 0.0]) # TODO change init or angle to minimize
-    position = jnp.matmul(rotate_z(5 * (txy[1]/resolution - 0.5)), position) # TODO make it follow more
-    position = position + jnp.array([0.0, 0.0, -10 * (txy[2]/resolution - 0.5)] )
-    direction = normalize(-position)
-    return position, direction
-
-
 sun_direction = normalize(jnp.array([1.0, 2.0, 3.0]))
 
 
-
 def ray_color(u, v, txy):
-    camera_position, camera_direction = get_camera(txy)
-    ray_direction = get_ray_direction(u, v, camera_direction)
-    distance = 0.0
+    time = txy[0]
+    x = txy[1]
+    y = txy[2]
 
+    camera_position, camera_direction = get_camera(x, y)
+    ray_direction = get_ray_direction(u, v, camera_direction)
+
+    # Marching
+    distance = 0.0
     for _ in range(20):
-        distance += distance_function(camera_position + ray_direction * distance, txy[0])
+        distance += distance_function(camera_position + ray_direction * distance, time)
 
     point_at_surface = camera_position + ray_direction * distance
-    normal_at_surface = jax.grad(distance_function)(point_at_surface, txy[0])
+
+    normal_at_surface = jax.grad(distance_function)(point_at_surface, time)
     normal_at_surface = normalize(normal_at_surface)
 
+    # Lighting
+    # Fog
     fog = jnp.clip(jnp.exp(- distance * distance *  0.005), 0.0, 1.0)
-
-    # Calculate specular reflection
+    # Specular
     reflected = reflect(-sun_direction, normal_at_surface)
     specular = jnp.maximum(0.0, jnp.dot(ray_direction, reflected)) ** 20.0
-
-    # Combine diffuse and specular lighting with fog
+    # Diffusion
     diffuse = (jnp.dot(normal_at_surface, sun_direction) + 1) / 2
+
     return (0.5 * diffuse + 0.5 * specular) * fog
 
 
